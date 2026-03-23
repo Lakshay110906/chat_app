a212641718e402282420fe7c81c67ba58bf15908
Lakshay Sharma <lakshaysharma110906@gmail.com>
2026-03-23 21:36:59 +0530

    server.js updated for vercel

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import http from 'http'
 import { connectDB } from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
 import messageRouter from './routes/messageRoutes.js';
-import {Server} from 'socket.io'
+import { Server } from 'socket.io'
 
 
 //Create Express app and HTTP server
@@ -14,23 +14,23 @@ const server = http.createServer(app)
 
 //Intialize socket.io server
 export const io = new Server(server, {
-    cors :{origin:"*"}
+    cors: { origin: "*" }
 })
 
 //Store Online users
 export const userSocketMap = {} //{userId : socketId}
 
 //Socket.io connection handler
-io.on('connection', (socket)=>{
+io.on('connection', (socket) => {
     const userId = socket.handshake.query.userId
     console.log('User connected', userId)
 
-    if(userId) userSocketMap[userId] = socket.id
+    if (userId) userSocketMap[userId] = socket.id
 
     //Emit online users to all connected clients
     io.emit('getOnlineUsers', Object.keys(userSocketMap))
-    
-    socket.on('disconnect', ()=>{
+
+    socket.on('disconnect', () => {
         console.log('User Disconnected', userId)
         delete userSocketMap[userId]
         io.emit('getOnlineUsers', Object.keys(userSocketMap))
@@ -38,16 +38,19 @@ io.on('connection', (socket)=>{
 })
 
 //Middleware SetUp
-app.use(express.json({limit:"4mb"}))
+app.use(express.json({ limit: "4mb" }))
 app.use(cors());
 
 //Routes Setup
-app.use('/api/auth/',userRouter)
-app.use('/api/messages',messageRouter)
-app.use('/',(req,res)=> res.send('Server is live'))
+app.use('/api/auth/', userRouter)
+app.use('/api/messages', messageRouter)
+app.use('/', (req, res) => res.send('Server is live'))
 
 //Connect to mongodb
 await connectDB()
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 7000
+    server.listen(PORT, () => console.log('Server is running on Port: ' + PORT))
+}
+export default server
 
-const PORT = process.env.PORT || 7000
-server.listen(PORT ,()=> console.log('Server is running on Port: ' + PORT))
